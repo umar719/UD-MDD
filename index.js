6e4358bc20ec7fa438d17c8c1c1bcdfb53f76f93
U M A R D E V I L <103053269+umar719@users.noreply.github.com>
2024-12-06 19:44:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -217,7 +217,7 @@ console.log("Session download completed !!")
     //====================================================================
 
         if(config.AOTO_VOICS == 'true' ) {
-  const url = 'https://gist.github.com/ALPHAkaveen/f0201bbbc2ca43f10a64aec2dd0d3c92/raw'
+  const url = 'https://raw.githubusercontent.com/Um4r719/UD-MD-DATABASE/refs/heads/main/UMAR_VOICE/CONNECTOR/UD-MD'
   let { data } = await axios.get(url)
   for (vr in data){
   if((new RegExp(`\\b${vr}\\b`,'gi')).test(body)) conn.sendMessage(from,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:mek})   
